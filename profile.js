934ac14bc940682110405e2524384fe2fad89dcd
ricardoalexand <126792060+ricardoalexand@users.noreply.github.com>
2023-11-29 22:39:09 +0000

    new changes

--- profile.js
+++ profile.js
@@ -7,10 +7,10 @@ let main = document.querySelector(".main");
 button.addEventListener("click", function() {
 
     // 1. Updated the innerHTML of profileName to your name or a made up one.
-
+  profileName.innerHTML = "Richard-Z";
     // 2. Updated the src of profileImage a picture of you or one that makes sense.
-
+  profileImage.src = "basket.png";
     // 3. Updated the src of main to a photo that's be your first post.
-
-
+  main.src = "curry.jpg";
+  
 });
--- stringInterpolation.js
+++ stringInterpolation.js
@@ -2,15 +2,16 @@
 let greeting = document.querySelector(".greeting");
 
 // 1. Create a variable called `firstName` and set the value to your name.
-
+let firstName = "Richard";
 
 // 2. Create a variable called `lastName` and set the value to your last name.
-
+let lastName = "Alex";
 
 
 // 3. Create a variable called `greetingMessage`.
 // - Using string interpolation, create a messga that say "Hi, (your first name) (your last name)!"
-
+let greetingMessage = `Hi, ${firstName} ${lastName}!;` 
 
 
 // 4. Set the innerHTML of the greeting div to the greeting message.
+greeting.InnerHTML = greetingMessage;
